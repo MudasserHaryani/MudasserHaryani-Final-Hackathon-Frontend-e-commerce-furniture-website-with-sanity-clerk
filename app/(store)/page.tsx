5758ca852fd6c81400199350e901185cb007c374
MudasserHaryani <Mudasser.haryani71@gmail.com>
2025-01-31 23:39:19 +0500

    first submission

---
+++ app/(store)/page.tsx
@@ -0,0 +1,22 @@
+import ProductsView from "@/components/ProductsView";
+import { Button } from "@/components/ui/button";
+import { getAllCategories } from "@/sanity/lib/products/getAllCategories";
+import { getAllProducts } from "@/sanity/lib/products/getAllProducts";
+
+export default async function Home() {
+  const products = await getAllProducts();
+const categories = await getAllCategories();
+ 
+  return (
+   <div>
+    <h1>hello world</h1>
+   
+   <div className="flex flex-col items-center justify-top min-h-screen bg-gray-100 p-4 ">
+    <ProductsView 
+    products={products} categories={categories}/>
+   </div>
+
+   </div>
+   
+  );
+}
---
+++ components/ProductsView.tsx
@@ -0,0 +1,29 @@
+import { Category, Product } from "@/sanity.types";
+import ProductGrid from "./ProductGrid";
+
+interface ProductsViewProps {
+  products: Product[];
+  categories: Category[];
+}
+
+const ProductsView = ({ products, categories }: ProductsViewProps) => {
+  return (
+    <div>
+      {/* categories */}
+      <div className="flex flex-col">
+        {/* <CategorySelectorComponent categories={categories}/> */}
+      </div>
+
+      {/* products */}
+      <div className="flex-1 ">
+        <div>
+          <ProductGrid products={products} />
+
+          <hr className="w-1/2 sm:w-3/4" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProductsView;
