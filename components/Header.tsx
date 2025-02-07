e7316f18ab36aede02e2b858d9c968418e33671a
MudasserHaryani <Mudasser.haryani71@gmail.com>
2025-02-08 00:16:14 +0500

    header

--- components/Header.tsx
+++ components/Header.tsx
@@ -79,7 +79,7 @@ const createClerkPassKey = async () => {
             <span className="absolute -top-2 -right-2 bg-red-500 text-white rounded-full w-5 h-5 flex items-center justify-center text-xs ">
               {itemCount}
             </span>
-            <span>My Cart</span>
+            <span>Cart</span>
           </Link>
 
           {/* User area */}
@@ -90,7 +90,7 @@ const createClerkPassKey = async () => {
               className="flex-1 relative flex justify-center sm:justify-start sm:flex-none items-center space-x-2 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
               >
               <PackageIcon className="w-6 h-6" />
-              <span>My Orders</span>
+              <span>Orders</span>
               </Link>
               </SignedIn>
 
